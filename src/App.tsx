36c6a778e14e00ee8b1a133e50896d6c032830ce
tyroneje <tyroneedwards08@yahoo.com>
2024-05-21 00:16:52 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+<div>
+  <span>Stock</span>
+  <br/>
+  <input type="text"/>
+</div>
+<div>
+  <span>Amount</span>
+  <br/>
+  <input type="text"/>
+</div>
+<div>
+  <button>Calculate</button>
+</div>
+    </>
+  )
+}
+
+export default App
