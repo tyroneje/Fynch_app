d29c2405118e3453f28d8778d57cf975f1f1d409
tyroneje <tyroneedwards08@yahoo.com>
2024-05-27 23:05:09 -0400

    added additional hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
+  //const [stock, setStock] = useState('');
+  //const [amount, setAmount] = useState(0);
+  const [calculations, setCalculations] = useState(null);
+
+  function calculateStockAmount() {
+    // call Fynch API
+    /**
+     * Returns {
+     * symbol: 'abc',
+     * price: 1.00,
+     * amountToPurchase: 50
+     * }
+     */
+
+  }
 
   return (
     <>
-<div>
-  <span>Stock</span>
-  <br/>
-  <input type="text"/>
-</div>
-<div>
-  <span>Amount</span>
-  <br/>
-  <input type="text"/>
-</div>
-<div>
-  <button>Calculate</button>
-</div>
+      <h1>Fynch</h1>
+      {/* <form action=""></form> */}
+      <div>
+        <span>Stock</span>
+        <br />
+        <input type="text" />
+      </div>
+      <br />
+      <div>
+        <span>Amount</span>
+        <br />
+        <input type="text" />
+      </div>
+      <br />
+      <div>
+        <button onClick={calculateStockAmount}>Calculate</button>
+      </div>
+      {calculations ?
+        (<>
+          <br />
+          <br />
+          <div>
+            <h4>Stock Name</h4>
+            <div>
+              <div>stock value($)</div>
+              <div>amount to purchase (#)</div>
+            </div>
+          </div>
+        </>) : null}
     </>
   )
 }
