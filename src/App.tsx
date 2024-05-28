03c4ad84570c1a7911d7804372a268b66ad484b4
tyroneje <tyroneedwards08@yahoo.com>
2024-05-28 13:26:24 -0400

    setting up react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { useForm } from "react-hook-form";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
   //const [stock, setStock] = useState('');
   //const [amount, setAmount] = useState(0);
   const [calculations, setCalculations] = useState(null);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit =  (data:any) => {console.log(data);}
 
+  console.log(watch("example"));
   function calculateStockAmount() {
     // call Fynch API
     /**
@@ -23,22 +27,24 @@ function App() {
   return (
     <>
       <h1>Fynch</h1>
-      {/* <form action=""></form> */}
-      <div>
-        <span>Stock</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <span>Stock</span>
+          <br />
+          <input type="text" {...register("example")} />
+        </div>
         <br />
-        <input type="text" />
-      </div>
-      <br />
-      <div>
-        <span>Amount</span>
+        <div>
+          <span>Amount</span>
+          <br />
+          <input type="text" {...register("exampleRequired", { required: true })} />
+        </div>
         <br />
-        <input type="text" />
-      </div>
-      <br />
-      <div>
-        <button onClick={calculateStockAmount}>Calculate</button>
-      </div>
+        <div>
+          <input type='submit'/>
+        </div>
+        {errors.exampleRequired && <span>This field is required</span>}
+      </form>
       {calculations ?
         (<>
           <br />
